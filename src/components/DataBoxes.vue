35bb68445e2e8842fb50107965ab2af29b7015b9
MrJ1M1 <razzaqovjamshidbek1@gmail.com>
2022-10-29 15:49:08 +0500

    I have built this project

---
+++ src/components/DataBoxes.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="grid md:grid-cols-2 gap-4">
+        <div class="shadow-md bg-blue-100 p-10 text-center rounded">
+            <h3 class="text-3xl text-blue-900 font-bold mb-4">Cases</h3>
+            <div class="text-2xl mb-4">
+                <span class="font-bold">New: {{numberWithCommas(stats.NewConfirmed)}}</span>
+            </div>
+            <div class="text-2xl mb-4">
+                <span class="font-bold">Totat: {{numberWithCommas(stats.TotalConfirmed)}}</span>
+            </div>
+        </div>
+
+        <div class="shadow-md bg-blue-300 p-10 text-center rounded">
+            <h3 class="text-3xl text-blue-900 font-bold mb-4">Deaths</h3>
+            <div class="text-2xl mb-4">
+                <span class="font-bold">New: {{numberWithCommas(stats.NewDeaths)}}</span>
+            </div>
+            <div class="text-2xl mb-4">
+                <span class="font-bold">Totat: {{numberWithCommas(stats.TotalDeaths)}}</span>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'DataBoxes',
+        props: ['stats'],
+        methods: {
+            numberWithCommas(x){
+                return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+            }
+        },
+    }
+</script>
